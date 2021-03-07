04b76ae152f8e918e4aff1b624f114eaa5fc4128
Matheus Sartori <ms.sartori@outlook.com.br>
2021-03-07 20:29:56 -0300

    Request + error handling

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,79 @@
-import React from 'react'
+import React, { useState, FormEvent } from 'react'
+import { FiChevronRight } from 'react-icons/fi'
+import api from '../../services/api'
 
-import { Title } from './styles'
+import logoImg from '../../assets/logo.svg'
+
+import { Title, Form, Repositories, Error } from './styles'
+
+interface Repository {
+  full_name: string
+  description: string
+  owner: {
+    login: string,
+    avatar_url: string
+  }
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('')
+  const [inputError, setInputError] = useState('')
+  const [repositories, setRepositories] = useState<Repository[]>([])
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault()
+
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório.')
+      return
+    }
+
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`)
+
+      const repository = response.data
+
+      setRepositories([...repositories, repository])
+      setNewRepo('')
+      setInputError('')
+    } catch (e) {
+      setInputError('Erro na busca por esse repositório.')
+    }
+  }
+
   return (
-    <Title>Explore repositórios no GitHub</Title>
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          placeholder="Digite o nome do repositório"
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      { inputError  && <Error>{inputError}</Error> }
+
+      <Repositories>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Repositories>
+    </>
   )
 }
 
